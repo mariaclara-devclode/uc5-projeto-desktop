28b6b0e3c7dd925badf988b3cbaac65d190ea71a
MARIA CLARA DE OLIVEIRA MAIA <a95833493@rn.senac.local>
2026-07-30 18:52:33 -0300

    Configuração da janela principal, DevTools e adição do menu da aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain } from 'electron'
+import { app, BrowserWindow, ipcMain, Menu } from 'electron'
 import path from 'path'
 
 let mainWindow: BrowserWindow | null = null
@@ -23,20 +23,67 @@ function createWindow() {
     },
   })
 
+
   // Se estiver em desenvolvimento, usa a URL do Vite. Em produção, carrega o arquivo compilado.
   if (process.env.VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(process.env.VITE_DEV_SERVER_URL)
-  } else {
-    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'))
-  }
+  mainWindow.loadURL(process.env.VITE_DEV_SERVER_URL)
+  mainWindow.webContents.openDevTools()
+} else {
+  mainWindow.loadFile(path.join(__dirname, '../dist/index.html'))
+}
+
+
+  mainWindow.once('ready-to-show', () => {
+
+    mainWindow?.show()
+
+  })
+
+}
+
+function createMenu() {
+  const menu = Menu.buildFromTemplate([
+    {
+      label: 'Gerenciador de Estoque Comercial',
+      submenu: [
+        {
+          label: 'Sobre',
+          click: () => {
+            console.log('Gerenciador de Estoque Comercial')
+          }
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'quit'
+        }
+      ]
+    },
+    {
+      label: 'Ajuda',
+      submenu: [
+        {
+          label: 'Versão',
+          click: () => {
+            console.log('Versão 1.0')
+          }
+        }
+      ]
+    }
+  ])
+
+  Menu.setApplicationMenu(menu)
 }
 
 app.whenReady().then(() => {
   createWindow()
+  createMenu()
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
+
     }
   })
 })
@@ -47,7 +94,13 @@ app.on('window-all-closed', () => {
   }
 })
 
+app.on('before-quit', () => {
+  console.log('Até logo!Encerrando o sistema...')
+})
+
+
 // Manipulador IPC Exemplo
 ipcMain.handle('canal-ping', async () => {
   return 'pong do processo principal!'
 })
+
