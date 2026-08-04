34d473f35948039ca15504858b11d755a626e595
MARIA CLARA DE OLIVEIRA MAIA <a95833493@rn.senac.local>
2026-08-03 21:09:21 -0300

    refactor: atualiza o arquivo preload.ts

--- src/preload.ts
+++ src/preload.ts
@@ -2,4 +2,7 @@ import { contextBridge, ipcRenderer } from 'electron'
 
 contextBridge.exposeInMainWorld('api', {
   ping: () => ipcRenderer.invoke('canal-ping'),
+
+listarProdutos: () => ipcRenderer.invoke("listar-produtos")
+
 })
