99fc0482175abb974efe8f41d6798883715e5fef
MARIA CLARA DE OLIVEIRA MAIA <a95833493@rn.senac.local>
2026-08-04 21:12:05 -0300

    Atualiza tipagem dos produtos com categoria e estoque

--- src/global.d.ts
+++ src/global.d.ts
@@ -11,9 +11,22 @@ declare global {
           nome: string;
           codigo_barras: string;
           preco_venda: number;
-          id_categoria: number;
-        }[]
-      >;
-    };
+          categoria: string;
+          estoque: number;
+         }[]
+      >
+
+      registrarMovimentacao(
+        id_produto: number,
+        quantidade: number,
+        tipo: 'entrada' | 'saida'
+      ): Promise<{
+        id: number
+        id_produto: number
+        quantidade: number
+        tipo: 'entrada' | 'saida'
+        data: string
+      }>
+    }
   }
 }
