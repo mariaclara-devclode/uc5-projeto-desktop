f9fb66b5a77a5ac00bd93f87c1b1f593a1ec0c6b
MARIA CLARA DE OLIVEIRA MAIA <a95833493@rn.senac.local>
2026-08-04 21:13:41 -0300

    feat: atualiza preload para listar produtos

--- src/preload.ts
+++ src/preload.ts
@@ -1,8 +1,24 @@
 import { contextBridge, ipcRenderer } from 'electron'
 
 contextBridge.exposeInMainWorld('api', {
+
   ping: () => ipcRenderer.invoke('canal-ping'),
 
-listarProdutos: () => ipcRenderer.invoke("listar-produtos")
+  listarProdutos: () =>
+    ipcRenderer.invoke('listar-produtos'),
+
+  registrarMovimentacao: (
+    id_produto: number,
+    quantidade: number,
+    tipo: 'entrada' | 'saida'
+  ) =>
+    ipcRenderer.invoke(
+      'registrar-movimentacao',
+      {
+        id_produto,
+        quantidade,
+        tipo
+      }
+    )
 
 })
