e3e9940cbd44ff6b591f7badb60d18f48857f127
Maria Clara <mc.2004mk@gmail.com>
2026-08-12 20:55:34 -0300

    Integração do menu de telas na inicialização

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,4 +1,10 @@
-import './style.css'
+import './interface'
+
+
+import {
+  configurarMenuTelas
+} from './telas'
+
 
 import {
   carregarProdutos,
@@ -6,77 +12,13 @@ import {
   carregarProdutosMovimentacao
 } from './produtos'
 
+
 import {
   carregarCategorias
 } from './categorias'
 
+
 import './movimentacoes'
-
-
-// MENU DE TELAS
-
-const menuTela =
-  document.getElementById(
-    'menu-tela'
-  ) as HTMLSelectElement
-
-
-const telas = {
-  produtos:
-    document.getElementById(
-      'tela-produtos'
-    ) as HTMLElement,
-
-  categorias:
-    document.getElementById(
-      'tela-categorias'
-    ) as HTMLElement,
-
-  cadastro:
-    document.getElementById(
-      'tela-cadastro'
-    ) as HTMLElement,
-
-  movimentacoes:
-    document.getElementById(
-      'tela-movimentacoes'
-    ) as HTMLElement
-}
-
-
-// TROCAR TELA
-
-function mostrarTela(
-  telaSelecionada: string
-) {
-
-  Object.values(telas).forEach(
-    (tela) => {
-      tela.style.display = 'none'
-    }
-  )
-
-  const tela =
-    telas[
-      telaSelecionada as keyof typeof telas
-    ]
-
-  if (tela) {
-    tela.style.display = 'block'
-  }
-}
-
-
-menuTela.addEventListener(
-  'change',
-  () => {
-
-    mostrarTela(
-      menuTela.value
-    )
-
-  }
-)
 
 
 // INICIALIZAÇÃO
@@ -85,17 +27,20 @@ async function iniciarSistema() {
 
   try {
 
+    configurarMenuTelas()
+
+
     await carregarCategorias()
+
 
     await carregarCategoriasNoProduto()
 
+
     await carregarProdutos()
+
 
     await carregarProdutosMovimentacao()
 
-    mostrarTela(
-      menuTela.value
-    )
 
     console.log(
       'Sistema iniciado com sucesso!'
@@ -109,9 +54,10 @@ async function iniciarSistema() {
     )
 
   }
-
 }
 
+
 iniciarSistema()
+
 
 export {}
