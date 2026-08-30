8ec7e284520a39f1c28c86e61127afd0c2335756
Maria Clara <mc.2004mk@gmail.com>
2026-08-29 21:05:23 -0300

    Centralização dos canais IPC

---
+++ src/canais.ts
@@ -0,0 +1,39 @@
+export const CANAIS = {
+
+  ping:
+    "canal-ping",
+
+  listarProdutos:
+    "listar-produtos",
+
+  buscarProdutos:
+    "buscar-produtos",
+
+  listarEstoqueCritico:
+    "listar-estoque-critico",
+
+  cadastrarProduto:
+    "cadastrar-produto",
+
+  editarProduto:
+    "editar-produto",
+
+  excluirProduto:
+    "excluir-produto",
+
+  listarCategorias:
+    "listar-categorias",
+
+  cadastrarCategoria:
+    "cadastrar-categoria",
+
+  editarCategoria:
+    "editar-categoria",
+
+  excluirCategoria:
+    "excluir-categoria",
+
+  registrarMovimentacao:
+    "registrar-movimentacao",
+
+} as const;
