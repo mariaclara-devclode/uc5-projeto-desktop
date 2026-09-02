39610ae1a94a474b28352e27db20cea71aa6381c
Maria Clara <mc.2004mk@gmail.com>
2026-09-02 20:17:39 -0300

    feat: adiciona status ativo ao produto

--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,22 @@
 export interface Categoria {
-  id: number
-  nome: string
-  descricao?: string
+  id: number;
+  nome: string;
+  descricao: string;
 }
 
 export interface Produto {
-  id: number
-  nome: string
-  codigo_barras?: string
-  preco_venda: number
-  id_categoria: number
+  id: number;
+  nome: string;
+  codigo_barras: string;
+  preco_venda: number;
+  id_categoria: number;
+  ativo: boolean;
 }
 
 export interface Movimentacao {
-  id: number
-  id_produto: number
-  quantidade: number
-  tipo: 'entrada' | 'saida'
-  data: Date
+  id: number;
+  id_produto: number;
+  quantidade: number;
+  tipo: "entrada" | "saida";
+  data: Date;
 }
